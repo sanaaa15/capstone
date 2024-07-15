30b50ace1ade8a7c76ac3077130154ccf14f4e35
Prad <PES1202102641@pesu.pes.edu>
2024-07-16 01:47:54 +0530

    fix:extend timeout

--- app/api/dbConfig.js
+++ app/api/dbConfig.js
@@ -1,9 +1,9 @@
-
 const dbConfig = {
   host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  connectTimeout: 60000 
 };
 
 export default dbConfig;
